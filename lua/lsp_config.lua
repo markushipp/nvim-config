75119c3eee9763ad357ce350a65b88add97a433d
markushipp <markus.hipp@mailbox.org>
2023-06-06 22:05:58 +0200

    enabled powershell_se LSP

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -1,7 +1,9 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = {"lua_ls", "gopls"}
+    ensure_installed = {"lua_ls", "gopls", "powershell_es"}
 })
 
 require("lspconfig").gopls.setup {}
 require("lspconfig").lua_ls.setup {}
+require("lspconfig").powershell_es.setup {}
+
