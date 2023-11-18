78d89f10733952887e499a5572d9478c8ef4c109
markushipp <markus.hipp@mailbox.org>
2023-11-18 15:32:51 +0100

    new keymaps

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -10,6 +10,11 @@ keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- General keymaps 
+keymap("n", "<leader>w", "<cmd>write<CR>", opts)
+keymap("n", "<leader>q", "<cmd>quit<CR>", opts)
+keymap("n", "<leader>pr", "<cmd>cd /Users/v0id/Documents/Projects<CR>", opts)
+keymap("n", "<leader>cf", "<cmd>cd /Users/v0id/.config<CR>", opts)
 -- nvim-tree keymaps
 
 keymap("n", "<leader>e", ":NvimTreeOpen<CR>", opts)
